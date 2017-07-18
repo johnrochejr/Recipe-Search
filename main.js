54308f2d8d1fb4ad5eab9dc3d084a6e0a4a2a49b
John Roche <roche.johnjr@gmail.com>
2017-07-18 14:42:11 -0400

    finished up JS

--- main.js
+++ main.js
@@ -10,19 +10,28 @@
 
  let searchresult = document.querySelector('.searchresult');
 
+
  let url = 'https://crossorigin.me/http://www.recipepuppy.com/api/?q=omelet';
 
  fetch(url).then(response => response.json()).then(data => {
+
    for (let i = 0; i < data.results.length; i++) {
      console.log(data);
+     let h1 = document.createElement('h1');
+     let anchor = document.createElement('a');
      let img = document.createElement('img');
 
-     img.src = data.results[i].thumbnail;
-      if (data.results[i].thumbnail === " ") {
-       img.src = 'http://via.placeholder.com/350x150'
+     if (data.results[i].thumbnail === "") {
+       img.src = 'http://via.placeholder.com/350x150';
      } else {
        img.src = data.results[i].thumbnail;
      }
-     searchresult.appendChild(img);
+
+     h1.textContent = data.results[i].title;
+     anchor.href = data.results[i].href;
+
+     anchor.appendChild(img)
+     anchor.appendChild(h1)
+     searchresult.appendChild(anchor);
    }
  })
