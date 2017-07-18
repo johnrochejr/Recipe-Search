b3089ae2a6316aa2a924420881e8496c505616bd
John Roche <roche.johnjr@gmail.com>
2017-07-18 17:20:15 -0400

    Finished JS I think

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
- // Select node
+ // Search bar
 
- // URL
+ let url = 'https://crossorigin.me/http://www.recipepuppy.com/api/?q=';
 
- // star wars fetch with data
 
- // thumbnail
- // Title
- // href
+ let searchbar = document.querySelector('.searchbar');
+
+  searchbar.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let searchBox = document.getElementById('searchBox');
+    console.log(searchBox.value);
+    url += searchBox.value;
+
+ // Search results (anchor, images, HREF)
 
  let searchresult = document.querySelector('.searchresult');
 
 
- let url = 'https://crossorigin.me/http://www.recipepuppy.com/api/?q=omelet';
 
- fetch(url).then(response => response.json()).then(data => {
+ fetch(url).then(response => response.json()).then((data) => {
 
    for (let i = 0; i < data.results.length; i++) {
-     console.log(data);
-     let h1 = document.createElement('h1');
+     let paragraph = document.createElement('p');
      let anchor = document.createElement('a');
      let img = document.createElement('img');
 
@@ -27,11 +30,12 @@
        img.src = data.results[i].thumbnail;
      }
 
-     h1.textContent = data.results[i].title;
+     paragraph.textContent = data.results[i].title;
      anchor.href = data.results[i].href;
 
      anchor.appendChild(img)
-     anchor.appendChild(h1)
+     anchor.appendChild(paragraph)
      searchresult.appendChild(anchor);
    }
  })
+})
